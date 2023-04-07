dfc0f1806316fd8809a7167af42f3516142f318f
Jens Könning <jenskonning@MBP-von-Jens.fritz.box>
2023-04-07 16:52:32 +0200

    fix routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/webpage_vue/' : import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
